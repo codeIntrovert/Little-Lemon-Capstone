660f1eba42a8f3fe31345651904f70f51e3f2fe9
Hasan <9131411681hasan@gmail.com>
2024-10-13 02:10:30 +0530

    minor fix

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,7 +5,7 @@ export default function Nav() {
 <nav className="bg-white border-gray-200 dark:bg-secondary">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="https://github.com/codeIntrovert/little-lemon-capstone" className="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src={logo} className="h-8" alt="Flowbite Logo" />
+      <img src={logo} className="h-8" alt="little lemon logo" />
      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Little Lemon</span>
   </a>
   <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
